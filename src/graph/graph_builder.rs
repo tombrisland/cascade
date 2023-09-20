b40337ff7eb0d2e602094166366e9c38458c231b
Tom Brisland <tombrisland@gmail.com>
2023-09-20 11:28:02 +0100

    re-implement graph builder

--- src/graph/graph_builder.rs
+++ src/graph/graph_builder.rs
@@ -1,70 +1,63 @@
-// use std::sync::Arc;
-//
-// use petgraph::Graph;
-// use petgraph::graph::NodeIndex;
-//
-// use crate::connection::Connection;
-// use crate::graph::graph::{CascadeGraph, GraphInternal};
-// use crate::processor::{Process, Processor};
-// use crate::producer::{Produce, Producer, ProducerConfig};
-//
-// // Represents a graph of the entire graph
-// pub struct CascadeGraphBuilder {
-//     graph_internal: GraphInternal,
-//
-//     // Last node added
-//     last_index: Option<NodeIndex>,
-// }
-//
-// impl CascadeGraphBuilder {
-//     pub fn new() -> CascadeGraphBuilder {
-//         CascadeGraphBuilder {
-//             graph_internal: Graph::new(),
-//             last_index: None,
-//         }
-//     }
-//
-//     // Add producer with no outgoing connections
-//     pub fn add_producer<T: 'static + Produce>(mut self, producer: T) -> CascadeGraphBuilder {
-//         let index = self
-//             .graph_internal
-//             .add_node(ComponentNode::Producer(Arc::new(Producer::new(
-//                 producer,
-//                 ProducerConfig {
-//                     schedule_per_second: 1,
-//                     concurrency: 1,
-//                 },
-//             ))));
-//
-//         // Set the last_index to enable connect_to_previous
-//         self.last_index = Some(index);
-//
-//         self
-//     }
-//
-//     // Add a processor connected to the previously defined Producer / Processor
-//     pub fn connect_to_previous<T: 'static + Process>(&mut self, processor: T) -> &CascadeGraphBuilder {
-//         if self.last_index.is_some() {
-//             let destination =
-//                 self.graph_internal
-//                     .add_node(ComponentNode::Processor(Arc::new(Processor::new(
-//                         processor,
-//                     ))));
-//
-//             let edge_id: String = format!("{}-{}", self.last_index.unwrap().index(), destination.index());
-//
-//             self.graph_internal
-//                 .add_edge(self.last_index.unwrap(), destination, Arc::new(Connection::new(edge_id, 10_000)));
-//
-//             self.last_index = Some(destination);
-//         }
-//
-//         self
-//     }
-//
-//     pub fn build(self) -> CascadeGraph {
-//         CascadeGraph {
-//             graph_internal: self.graph_internal,
-//         }
-//     }
-// }
+use petgraph::Graph;
+use petgraph::graph::NodeIndex;
+
+use crate::component::definition::ComponentDefinition;
+use crate::connection::definition::ConnectionDefinition;
+use crate::graph::graph::{CascadeGraph, GraphInternal};
+
+// Represents a graph of the entire graph
+pub struct _CascadeGraphBuilder {
+    graph_internal: GraphInternal,
+
+    // Last node added
+    last_index: Option<NodeIndex>,
+}
+
+impl _CascadeGraphBuilder {
+    pub fn new() -> _CascadeGraphBuilder {
+        _CascadeGraphBuilder {
+            graph_internal: Graph::new(),
+            last_index: None,
+        }
+    }
+
+    // Add component with no outgoing connections
+    pub fn add_component(
+        mut self,
+        def: ComponentDefinition,
+    ) -> _CascadeGraphBuilder {
+        let index = self.graph_internal.add_node(def);
+
+        // Set the last_index to enable connect_to_previous
+        self.last_index = Some(index);
+
+        self
+    }
+
+    // Add a component connected to the previous component
+    pub fn connect_to_previous(
+        &mut self,
+        def: ComponentDefinition,
+    ) -> &_CascadeGraphBuilder {
+        if self.last_index.is_some() {
+            let source: NodeIndex = self.last_index.unwrap();
+            let destination: NodeIndex = self.graph_internal.add_node(def);
+
+            self.graph_internal.add_edge(
+                source.clone(),
+                destination,
+                ConnectionDefinition::new(source.index(), destination.index()),
+            );
+
+            self.last_index = Some(destination);
+        }
+
+        self
+    }
+
+    pub fn build(self) -> CascadeGraph {
+        CascadeGraph {
+            graph_internal: self.graph_internal,
+        }
+    }
+}
--- src/server/endpoint/http_util.rs
+++
@@ -1,8 +0,0 @@
-use hyper::{Body, Response, StatusCode};
-
-pub fn response(status: StatusCode, str: String) -> Response<Body> {
-    Response::builder()
-        .status(status)
-        .body(Body::from(str))
-        .unwrap()
-}
